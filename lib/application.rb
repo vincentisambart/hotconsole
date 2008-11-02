59894ff6881f396344ea5049da8a90f8b290a1f2
Vincent Isambart <vincent.isambart@gmail.com>
2008-11-02 12:19:22 +0900

    starts to look like irb

--- lib/application.rb
+++ lib/application.rb
@@ -3,8 +3,9 @@ framework 'webkit'
 
 # TODO:
 # - autoscroll
-# - focus
 # - stdout/stderr, later stdin
+# - wraps too long text
+# - allows Alt+Return
 class Application
   include HotCocoa
   
@@ -14,28 +15,17 @@ class Application
     
     def base_html
       return <<-HTML
+      <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
       <html>
       <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
         <style type="text/css"><!--
+          * {
+            font-family: Monaco;
+          }
         --></style>
       </head>
-      <body>
-        <div id="display"></div>
-        <table id="input_table">
-          <tr>
-            <td style="vertical-align: top;">&gt;&gt;</td>
-            <td style="width: 100%;"><div id="command_line" contentEditable="true"></div></td>
-          </tr>
-        </table>
-        <script type="text/javascript"><!--
-        // TODO: do it in Ruby (but must wait for the document to be loaded)
-        var command_line = document.getElementById('command_line');
-        command_line.focus();
-        //var selection = window.getSelection();
-        //selection.setBaseAndExtent(command_line, 0, command_line, command_line.innerText.length);
-        //selection.deleteFromDocument();
-        --></script>
-      </body>
+      <body></body>
       </html>
       HTML
     end
@@ -49,16 +39,14 @@ class Application
         @web_view = web_view(:layout => {:expand => [:width, :height]})
         @web_view.mainFrame.loadHTMLString base_html, baseURL:nil
         @web_view.editingDelegate = self
+        @web_view.frameLoadDelegate = self
         win << @web_view
       end
-#        @display = label(:text => '', :layout => {:expand => [:width, :height]})
-#        @command_line = text_field(:layout => {:expand => [:width]})
-#        win << @command_line
-#        win << @display
-#        @command_line.on_action do
-#          self.perform_action
-#        end
     end
+  end
+  
+  def webView view, didFinishLoadForFrame: frame
+    write_prompt
   end
   
   def document
@@ -71,24 +59,22 @@ class Application
   
   def webView webView, shouldInsertText: text, replacingDOMRange: range, givenAction: action
     if text == ?\n
-      #alert :message => "This is an alert!", :info => "#{command_line}"
       perform_action
       false
     else
       true
     end
-    #alert :message => "This is an alert!", :info => "#{text.unpack('U*')}"
   end
   
   def add_div(text)
     doc = document
     div = doc.createElement('div')
     div.innerText = text
-    doc.getElementById('display').appendChild(div)
+    doc.body.appendChild(div)
   end
   
   def write_element(element)
-    document.getElementById('display').appendChild(element)
+    document.body.appendChild(element)
   end
   
   def write_text(text)
@@ -97,14 +83,18 @@ class Application
     write_element(div)
   end
 
-  def write_old_prompt(text)
+  def write_prompt
     table = document.createElement('table')
     row = table.insertRow(0)
     prompt = row.insertCell(-1)
     prompt.innerText = '>>'
     typed_text = row.insertCell(-1)
-    typed_text.innerText = text
+    command_line.setAttribute('id', value: nil) if command_line
+    typed_text.setAttribute('contentEditable', value: 'true')
+    typed_text.setAttribute('id', value: 'command_line')
+    typed_text.setAttribute('style', value: 'width: 100%;')
     write_element(table)
+    command_line.focus
   end
   
   def scroll_to_bottom
@@ -115,10 +105,14 @@ class Application
 
   def perform_action
     @line_num += 1
+    #nbsp = ' '
     command = command_line.innerText
-    write_old_prompt(command)
-    command_line.innerText = ''
-    return if command.empty?
+    #the following line does not work due to bugs in MacRuby
+    #command = command.gsub(nbsp, ' ')
+    if command.empty?
+      write_prompt
+      return
+    end
 
     eval_file = __FILE__
     eval_line = -1
@@ -129,9 +123,14 @@ class Application
     rescue Exception => e
       backtrace = e.backtrace
       i = backtrace.index { |l| l.index("#{eval_file}:#{eval_line}") }
-      backtrace = backtrace[0..i-1] if i
-      add_div("#{e.class.name}: #{e.message}\n#{backtrace.join("\n")}")
+      if i == 0
+        backtrace = []
+      elsif i
+        backtrace = backtrace[0..i-1]
+      end
+      add_div("#{e.class.name}: #{e.message}" + (backtrace.empty? ? '' : "\n#{backtrace.join("\n")}"))
     end
+    write_prompt
     scroll_to_bottom
   end
   
